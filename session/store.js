cb04bc642f2f0104e83ed06137ccb653cfbe2dd3
Maxim Savin <soulman.is.good@gmail.com>
2015-02-12 02:44:07 +0600

    Added regeneration async logic

--- session/store.js
+++ session/store.js
@@ -39,8 +39,9 @@ Store.prototype.__proto__ = EventEmitter.prototype;
 Store.prototype.regenerate = function(req, fn){
   var self = this;
   this.destroy(req.sessionID, function(err){
-    self.generate(req);
-    fn(err);
+    self.generate(req, function(req) {
+      fn(err);
+    });
   });
 };
 
